00b9ce74b751824aeaaae09bd8399214d8e44024
odevpedro <pedrosschmidt2@gmail.com>
2023-05-19 14:26:20 -0300

    açszdf

---
+++ src/main/java/br/com/odevpedro/javasqs/JavasqsApplication.java
@@ -0,0 +1,13 @@
+package br.com.odevpedro.javasqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasqsApplication.class, args);
+	}
+
+}
